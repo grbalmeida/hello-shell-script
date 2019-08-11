97754873c283bee6f4f1fc07768a1c8e6cf86de0
gilvan <g.r.almeida@live.com>
2019-08-10 23:17:44 -0300

    Comments

--- creating-and-running-a-shell-script/first-script.sh
+++ creating-and-running-a-shell-script/first-script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#################################################################################
+#                                                                               #
+# first-script.sh - First script                                                #
+#                                                                               #
+# Author: Gilvan Ribeiro                                                        #
+# Creation date: 2019-08-10                                                     #
+#                                                                               #
+# Description: Script showing current date and time and displaying student list #
+#                                                                               #
+# Example of use: ./first-script.sh                                             #
+#                                                                               #
+#################################################################################
+
 clear
 echo "====== My first script ======"
 echo ""
