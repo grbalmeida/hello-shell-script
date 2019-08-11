731f54bbc04fd52bb41b7fd0575e86f9f062ee3f
gilvan <g.r.almeida@live.com>
2019-08-10 23:05:13 -0300

    First script

---
+++ creating-and-running-a-shell-script/first-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "====== My first script ======"
+echo ""
+echo -n "Display current date and time"
+echo ""
+date
+echo ""
+echo "====== Student listing ======"
+echo ""
+sort files/students.txt
